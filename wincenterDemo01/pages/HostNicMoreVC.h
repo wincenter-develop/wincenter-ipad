06c9630feab7d5e123ee422bbe7a04a4707e69c2
winhong2 <huadi@huadideiMac.local>
2014-08-27 17:07:29 +0800

    新增查看更多页

---
+++ wincenterDemo01/pages/HostNicMoreVC.h
@@ -0,0 +1,13 @@
+//
+//  HostNicMoreVC.h
+//  wincenterDemo01
+//
+//  Created by huadi on 14-8-27.
+//  Copyright (c) 2014年 huadi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HostNicMoreVC : UIViewController
+
+@end

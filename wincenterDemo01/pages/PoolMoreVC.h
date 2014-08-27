35f06b3f31bf602d7c39cbf465488e32b163ca8c
winhong2 <huadi@huadideiMac.local>
2014-08-27 15:58:00 +0800

    新增资源池“查看更多”页面

---
+++ wincenterDemo01/pages/PoolMoreVC.h
@@ -0,0 +1,13 @@
+//
+//  PoolMoreVC.h
+//  wincenterDemo01
+//
+//  Created by huadi on 14-8-27.
+//  Copyright (c) 2014年 huadi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PoolMoreVC : UIViewController
+
+@end
